355243eb05a3e64e2a427cd566b1e94cf95786b8
Patrick Ma <mapeiyuan1874@gmail.com>
2020-07-31 12:00:29 -0400

    1. add mybatis using case: http://127.0.0.1:9001/system/test
    2. add three layers

--- system/src/main/java/com/course/system/SystemApplication.java
+++ system/src/main/java/com/course/system/SystemApplication.java
@@ -1,5 +1,6 @@
 package com.course.system;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
@@ -9,6 +10,7 @@ import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 @EnableEurekaClient
+@MapperScan("com.course.system.mapper")
 public class SystemApplication {
 	private static final Logger LOG = LoggerFactory.getLogger(SystemApplication.class);
 	public static void main(String[] args) {
@@ -19,30 +21,3 @@ public class SystemApplication {
 	}
 }
 
-//package com.course;
-//
-//import org.slf4j.Logger;
-//import org.slf4j.LoggerFactory;
-//import org.springframework.boot.SpringApplication;
-//import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-//import org.springframework.core.env.Environment;
-//
-//@SpringBootApplication
-//@EnableEurekaServer
-//public class EurekaApplication {
-//
-//	private static final Logger LOG = LoggerFactory.getLogger(EurekaApplication.class);
-//
-////	public static void main(String[] args) {
-////		SpringApplication.run(EurekaApplication.class, args);
-////	}
-//
-//	public static void main(String[] args) {
-//		SpringApplication app = new SpringApplication(EurekaApplication.class);
-//		Environment env = app.run(args).getEnvironment();
-//		LOG.info("启动成功！！");
-//		LOG.info("Eureka地址: \thttp://127.0.0.1:{}", env.getProperty("server.port"));
-//	}
-//
-//}
