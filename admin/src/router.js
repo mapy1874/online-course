f0ef9b212be62593d90869ea80dbc63d6e00f1fc
Patrick Ma <mapeiyuan1874@gmail.com>
2020-08-01 16:25:39 -0400

    add welcome child component, add /admin/welcome child route

--- admin/src/router.js
+++ admin/src/router.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Login from "./views/login.vue"
 import Admin from "./views/admin.vue"
+import Welcome from "./views/admin/welcome.vue"
 
 Vue.use(Router);
 
@@ -17,5 +18,10 @@ export default new Router({
     },{
         path:"/admin",
         component:Admin,
+        children: [{
+            // do not add '/' in child route
+            path: 'welcome',
+            component: Welcome,
+        }]
     }]
 })
