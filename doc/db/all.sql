4bf80562ff8b03984a5caf758a451a8b8c24d388
Patrick Ma <mapeiyuan1874@gmail.com>
2020-08-01 20:38:13 -0400

    add sql chapter, generate codes for persistence layer

--- doc/db/all.sql
+++ doc/db/all.sql
@@ -1,3 +1,12 @@
+drop table if exists `chapter`;
+create table `chapter` (
+    `id` char(8) not null comment 'ID',
+    `course_id` char(8) comment 'course id',
+    `name` varchar(50) comment 'name',
+    primary key (`id`)
+) engine=innodb default charset=utf8mb4 comment 'chapter';
+
+# test
 drop table if exists `test`;
 
 create table `test` (
