9b747ca892d64ba367985745c726d565aed0f272
Patrick Ma <mapeiyuan1874@gmail.com>
2020-08-01 23:47:33 -0400

    1. integrate axios to have frontend interact with backend 2. Add CorsConfig.java to solve the cross origin problem

--- admin/src/main.js
+++ admin/src/main.js
@@ -1,10 +1,13 @@
 import Vue from 'vue'
 import App from './app.vue'
 import router from './router'
+import axios from 'axios'
 
 Vue.config.productionTip = false
+// $ represent the global attr of Vue
+Vue.prototype.$ajax = axios;
 
 new Vue({
   router,
   render: h => h(App),
-}).$mount('#app')
+}).$mount('#app');
