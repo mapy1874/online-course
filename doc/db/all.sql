e33c5d6679c817258a14cea7315978c348aca77e
Patrick Ma <mapeiyuan1874@gmail.com>
2020-08-01 21:06:44 -0400

    1. add dto for controller and service layer 2. add ChapterDto 3. Revise ChapterService, change the return type of Chapter into ChapterDto

--- doc/db/all.sql
+++ doc/db/all.sql
@@ -6,6 +6,10 @@ create table `chapter` (
     primary key (`id`)
 ) engine=innodb default charset=utf8mb4 comment 'chapter';
 
+insert into `chapter` (id, course_id, name)
+values ('00000000', '00000000', 'test chapter 1'),
+       ('00000001', '00000000', 'test chapter 2');
+
 # test
 drop table if exists `test`;
 
