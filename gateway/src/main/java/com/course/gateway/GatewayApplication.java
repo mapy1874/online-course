ae80856bbce05de33294046182f90e06d4a02dd7
Patrick Ma <mapeiyuan1874@gmail.com>
2020-08-03 14:04:44 -0400

    use lb://business for route forwarding

--- gateway/src/main/java/com/course/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/course/gateway/GatewayApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
