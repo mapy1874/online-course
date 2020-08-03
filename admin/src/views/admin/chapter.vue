f99859eb60276ae859483cb8c0eec96108ebbfbc
Patrick Ma <mapeiyuan1874@gmail.com>
2020-08-03 11:01:11 -0400

    use gateway to forward routers, page only visit gateway's interface

--- admin/src/views/admin/chapter.vue
+++ admin/src/views/admin/chapter.vue
@@ -88,7 +88,7 @@
     methods: {
       list() {
         let _this = this;
-        _this.$ajax.get('http://127.0.0.1:9002/business/admin/chapter/list').then(response => {
+        _this.$ajax.get('http://127.0.0.1:9000/business/admin/chapter/list').then(response => {
           console.log("query chapter result: ", response);
           _this.chapters = response.data;
         })
