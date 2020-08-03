53927e6b800f3464b25398aeb36c66d7a8eb42d0
Patrick Ma <mapeiyuan1874@gmail.com>
2020-08-03 15:13:35 -0400

    pass page parameters into backend

--- admin/src/views/admin/chapter.vue
+++ admin/src/views/admin/chapter.vue
@@ -88,9 +88,12 @@
     methods: {
       list() {
         let _this = this;
-        _this.$ajax.get('http://127.0.0.1:9000/business/admin/chapter/list').then(response => {
+        _this.$ajax.post('http://127.0.0.1:9000/business/admin/chapter/list', {
+          page: 1,
+          size: 1,
+        }).then(response => {
           console.log("query chapter result: ", response);
-          _this.chapters = response.data;
+          _this.chapters = response.data.list;
         })
       }
     }
---
+++ server/src/main/java/com/course/server/dto/PageDto.java
@@ -0,0 +1,46 @@
+package com.course.server.dto;
+
+import java.util.List;
+
+public class PageDto<T> {
+    //    page number
+    protected int page;
+    // numbers in each page
+    protected int size;
+
+    protected long total;
+    protected List<T> list;
+
+    public int getPage() {
+        return page;
+    }
+
+    public void setPage(int page) {
+        this.page = page;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public long getTotal() {
+        return total;
+    }
+
+    public void setTotal(long total) {
+        this.total = total;
+    }
+
+    public List<T> getList() {
+        return list;
+    }
+
+    public void setList(List<T> list) {
+        this.list = list;
+    }
+}
+
