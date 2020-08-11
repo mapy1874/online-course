5df25e6ce8545ff962e9c8aac45120e87cce174c
Patrick Ma <mapeiyuan1874@gmail.com>
2020-08-11 09:08:03 -0400

    1. add EnumGenerator 2. change the filter of optionKV from list into object

--- admin/public/static/js/enums.js
+++ admin/public/static/js/enums.js
@@ -1,1 +1,2 @@
-CHARGE = [{key: "C", value:"Charge"}, {key:"F", value: "Free"}];
+SECTION_CHARGE={CHARGE:{key:"C", value:"Charge"},FREE:{key:"F", value:"Free"}};
+YES_NO={YES:{key:"1", value:"是"},NO:{key:"0", value:"否"}};
---
+++ business/src/main/java/com/course/business/controller/admin/SectionController.java
@@ -0,0 +1,61 @@
+package com.course.business.controller.admin;
+
+import com.course.server.domain.Section;
+import com.course.server.dto.SectionDto;
+import com.course.server.dto.PageDto;
+import com.course.server.dto.ResponseDto;
+import com.course.server.service.SectionService;
+import com.course.server.util.ValidatorUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.validation.ValidationUtils;
+import org.springframework.validation.Validator;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@RestController
+@RequestMapping("/admin/section")
+public class SectionController {
+
+    private static final Logger LOG = LoggerFactory.getLogger(SectionController.class);
+    public static final String BUSINESS_NAME = "section";
+
+    @Resource
+    private SectionService sectionService;
+
+    // section query
+    @PostMapping("/list")
+    public ResponseDto list(@RequestBody PageDto pageDto) {
+        LOG.info("pageDto: {}", pageDto.getPage());
+
+        ResponseDto responseDto = new ResponseDto();
+        sectionService.list(pageDto);
+        responseDto.setContent(pageDto);
+        return responseDto;
+    }
+
+    // save, update when id have value, create when id is null
+    @PostMapping("/save")
+    public ResponseDto save(@RequestBody SectionDto sectionDto) {
+        // validation in saving
+            ValidatorUtil.require(sectionDto.getTitle(), "title");
+                ValidatorUtil.length(sectionDto.getTitle(), "title", 1, 50);
+                ValidatorUtil.length(sectionDto.getVideo(), "video", 1, 200);
+
+        ResponseDto responseDto = new ResponseDto();
+        sectionService.save(sectionDto);
+        responseDto.setContent(sectionDto);
+        return responseDto;
+    }
+
+    // delete
+    @DeleteMapping("/delete/{id}")
+    public ResponseDto delete(@PathVariable String id) {
+        ResponseDto responseDto = new ResponseDto();
+        sectionService.delete(id);
+        return responseDto;
+    }
+
+}
